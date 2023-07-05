901ac1f74c8addf944aee4863b3893799554c9ad
alexandergetka <GetkaA@pennmedicine.upenn.edu>
2023-07-05 19:55:32 -0400

    Initial commit of nichart portal

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cbicanicharthelloworld": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
